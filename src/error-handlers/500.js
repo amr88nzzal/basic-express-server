d644c820dcfe5515deaef14bde40ebf4478b108a
AMR NZZAL <amr.nzzal@gmail.com>
2021-11-04 04:07:50 +0200

    final

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function err500(req, res, next) {
-  res.status(500).send('Error 500**');
+function err500(err,req, res, next) {
+  res.status(500).send(`Error (500) : Invalid Query\n${err}`);
 
 }
 
